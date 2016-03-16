85a4be71208d5436a4f05df65d9c80de217c6782
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-03-16 10:51:42 +0100

    moved stuff

---
+++ project/src/test/TestDataReader.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.Data.DataReader import DataReader
+
+
+class TestDataReader(unittest.TestCase):
+
+    def test_read_data(self):
+        filename = '../../data/subject1_csv/eeg_200605191428_epochs.csv'
+
+        dataset = DataReader.read_data(filename, ',')
+
+        print dataset.principal_component(3)
