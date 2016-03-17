d3d0f5c02e8d5f7c5823abffe99065f1236e5e33
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-03-17 13:37:58 +0100

    projekt

--- project/src/test/TestDataSet.py
+++ project/src/test/TestDataSet.py
@@ -9,11 +9,14 @@ class TestDataSet(unittest.TestCase):
         filename = '../../data/subject1_csv/eeg_200605191428_epochs.csv'
 
         dataset = DataReader.read_data(filename, ',')
+        # Add random noise to 3 randomly chosen columns
         dataset.add_artifacts(3)
 
         pca_dataset = dataset.principal_component(k=None, component_variance=0.95)
 
         print pca_dataset[0].params
+
+        # TODO: Project the principal components back to the original dataset
 
         plt.plot(pca_dataset.unpack_params())
         plt.ylabel('Principal components')
