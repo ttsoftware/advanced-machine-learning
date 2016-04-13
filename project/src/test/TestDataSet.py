319f3f78dd0652ae92d112f251acaa8a4bef0b56
RasmusHaarslev <rasmus.haarslev@gmail.com>
2016-04-13 18:28:13 +0200

    Added testing support for simulating real time data feed.

--- project/src/test/TestDataSet.py
+++ project/src/test/TestDataSet.py
@@ -2,17 +2,21 @@ import unittest
 
 from src.data.DataReader import DataReader
 from src.artifacts.Artificer import Artificer
+from src.data.DataSet import DataSet
 
 
 class TestDataSet(unittest.TestCase):
     def test_pca(self):
 
         filename = '../../data/subject1_csv/eeg_200605191428_epochs/small.csv'
-        filename_artifacts = '../../data/subject1_csv/eeg_200605191428_epochs/tiny_artifacts.csv'
 
         dataset = DataReader.read_data(filename, ',')
 
-        artificer = Artificer(dataset)
-        artificer.pca_reconstruction()
+        for idx in range(len(dataset) // 40):
+            current_dataset = DataSet(dataset[idx*40:(idx+1)*40])
 
-        artificer.visualize()
+            artificer = Artificer(current_dataset)
+            artificer.pca_reconstruction()
+
+            # TODO: What do we want to do with each window?
+
