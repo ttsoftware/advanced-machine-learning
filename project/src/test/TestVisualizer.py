26a357625a8ec63ea09fc230fb44dfcd0b542c4d
RasmusHaarslev <rasmus.haarslev@gmail.com>
2016-05-12 12:41:16 +0200

    test

--- project/src/test/TestVisualizer.py
+++ project/src/test/TestVisualizer.py
@@ -23,10 +23,10 @@ class TestDataSet(unittest.TestCase):
         reconstructed_dataset_avg, rejections = ExperimentorService.pca_reconstruction(artifact_dataset, window_size, threshold_avg)
         reconstructed_dataset_avg_max, rejections = ExperimentorService.pca_reconstruction(artifact_dataset, window_size, threshold_avg_max)
 
-        # Visualizer.visualize_mse(test_set, reconstructed_dataset_max, window_size, 'figure_mse_max')
-        # Visualizer.visualize_mse(test_set, reconstructed_dataset_avg, window_size, 'figure_mse_avg')
-        # Visualizer.visualize_mse(test_set, reconstructed_dataset_avg_max, window_size, 'figure_mse_avg_max')
+        Visualizer.visualize_mse(test_set, reconstructed_dataset_max, window_size, 'figure_mse_max')
+        Visualizer.visualize_mse(test_set, reconstructed_dataset_avg, window_size, 'figure_mse_avg')
+        Visualizer.visualize_mse(test_set, reconstructed_dataset_avg_max, window_size, 'figure_mse_avg_max')
 
-        thresholds = [threshold_max, threshold_avg, threshold_avg_max]
-        window_sizes = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-        Visualizer.visualize_cross_validation(test_set, artifact_dataset, thresholds, window_sizes)
+        # thresholds = [threshold_max, threshold_avg, threshold_avg_max]
+        # window_sizes = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+        # Visualizer.visualize_cross_validation(test_set, artifact_dataset, thresholds, window_sizes)
